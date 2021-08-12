86a6e697484647baee8fed926a30a955d8d8b037
lislis <mail@lislis.de>
2021-08-12 14:57:11 +0200

    new data

--- example-2.js
+++ example-2.js
@@ -1,18 +1,18 @@
-var mymap = L.map('mapid').setView([52.5163, 13.3779], 13);
+var mymap = L.map('mapid').setView([52.5163, 13.3779], 10);
 
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
   maxZoom: 18
 }).addTo(mymap);
 
-window.fetch('berliner-toiletten-standorte-clean.csv')
+window.fetch('eladesaeulen.csv')
   .then((response) => response.text())
   .then((data) => {
     var rows = data.split('\n').slice(1).map(x => x.split(','));
 
     rows.forEach((x) => {
-      var lat =  parseFloat(x[5]);
-      var lon = parseFloat(x[4]);
+      var lat =  parseFloat(x[9]);
+      var lon = parseFloat(x[10]);
       if (lat && lon) {
         L.circle([lat, lon], {
 	  color: '#ac00e6',
